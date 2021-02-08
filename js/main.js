e32b51016171e6fd0fd10e2f166954fdb2a122fe
rajendraf5 <68588129+rajendraf5@users.noreply.github.com>
2021-02-08 14:38:15 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function( $ ) {
 
-  
-  // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
@@ -14,15 +12,6 @@ jQuery(document).ready(function( $ ) {
     return false;
   });
 
-// $('.page-scroll').click(function(e){
-//   e.preventDefault();
-//   var target = $($(this).attr('href'));
-//   if(target.length){
-//     var scrollTo = target.offset().top -100;
-//     $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
-//   }
-// });
-   
 
     $('.page-scroll').click(function() {
       $('.navbar-collapse').collapse('hide');
@@ -42,40 +31,6 @@ jQuery(document).ready(function( $ ) {
         }
     });
   
-
-  // Intro carousel
-  // var introCarousel = $(".carousel");
-  // var introCarouselIndicators = $(".carousel-indicators");
-  // introCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
-  //   (index === 0) ?
-  //   introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "' class='active'></li>") :
-  //   introCarouselIndicators.append("<li data-target='#introCarousel' data-slide-to='" + index + "'></li>");
-
-  //   $(this).css("background-image", "url('" + $(this).children('.carousel-background').children('img').attr('src') +"')");
-  //   $(this).children('.carousel-background').remove();
-  // });
-
-  // $(".carousel").swipe({
-  //   swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-  //     if (direction == 'left') $(this).carousel('next');
-  //     if (direction == 'right') $(this).carousel('prev');
-  //   },
-  //   allowPageScroll:"vertical"
-  // });
-
- 
-  // Clients carousel (uses the Owl Carousel library)
-  // $(".activity-carousel").owlCarousel({
-  //   autoplay: false,
-  //   nav: true,
-  //   dots: false,
-  //   loop: true,
-  //   navText : ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
-  //   responsive: { 0: { items: 1 }, 768: { items: 1 }, 900: { items: 4 }
-  //   }
-  // });
-
-
   /* - Expand Panel * */
     $("#slideit").on ("click", function() {
       $("#slidepanel").slideDown(1000);
@@ -168,6 +123,18 @@ jQuery(document).ready(function( $ ) {
     responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 3 }
     }
   });
+
+    $(function(){
+    var $page = jQuery.url.attr("file");
+    $('ul.nav-menu li a').each(function(){
+      var $href = $(this).attr('href');
+      if ( ($href == $page) || ($href == '') ) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+  });
 });
 
  // stickey menu
@@ -193,3 +160,31 @@ jQuery(document).ready(function( $ ) {
         }, 1250, "easeInOutExpo"), b.preventDefault();
     });
 }(jQuery);
+
+( function ( $ ) {
+    'use strict';
+  $('#listtabs').on('click', '.tablink,#prodTabs a', function (e) {
+    e.preventDefault();
+    window.location.hash = this.hash;
+    $(this).tab('show');
+    $(window).scrollTop(0);
+  });
+   
+  $('ul#stmenu a').on('click', function (e) {       
+    window.location.hash = this.hash;
+    var hash = window.location.hash;    
+    hash && $('ul#prodTabs a[href="' + hash + '"]').tab('show');
+    if (location.hash) {               // do the test straight away
+      window.scrollTo(0, 0);         // execute it straight away
+      setTimeout(function () {
+               window.scrollTo(0, 0);     // run it a bit later also for browser compatibility
+           }, 1);
+    }   
+  });
+   
+  $(document).ready(() => {
+    var hash = window.location.hash;
+    hash && $('ul#prodTabs a[href="' + hash + '"]').tab('show');
+    $(window).scrollTop(0);
+  }); 
+} ( jQuery ) )
